f3889800d9e8f7415fcbe502d6891dee938e420e
BharathKumarReddy <nbkumar2103@gmail.com>
2026-09-23 18:23:58 +0530

    updated file

---
+++ agenticai/openai_n_claude_api/gpt.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+
+def interact_with_model():
+    load_dotenv()
+    client = OpenAI(
+        api_key=os.getenv("OPENAI_API_KEY")
+    )
+    response = client.responses.create(
+        model="gpt-5.6-luna",
+        input="What is the capital of France?"
+    )
+    print(response)
---
+++ agenticai/openai_n_claude_api/main.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+import claude
+import gpt
+
+
+# def print_api_keys():
+#     load_dotenv()  # Load environment variables from .env file
+#     print(os.getenv("ANTHROPIC_API_KEY"))
+#     print(os.getenv("OPENAI_API_KEY"))
+
+
+def interact_with_claude_model():
+    pass  # Placeholder for the function to interact with Claude model
+
+
+if __name__ == "__main__":
+    #print_api_keys()
+    claude.interact_with_model()
+    #gpt.interact_with_model()
