d9eb87963c2b4655ec1c555dc79f4838a7e04d90
BharathKumarReddy <nbkumar2103@gmail.com>
2026-09-25 22:57:01 +0530

    updated file

--- agenticai/openai_n_claude_api/claude.py
+++ agenticai/openai_n_claude_api/claude.py
@@ -1,18 +1,78 @@
 from anthropic import Anthropic
+from anthropic.types import Message
 from dotenv import load_dotenv
 import os
 
-def interact_with_model():
-    load_dotenv()  # Load environment variables from .env file
+def ask_claude(question: str):
+    load_dotenv()
     client = Anthropic(
-        api_key=os.getenv("ANTHROPIC_API_KEY")
+        api_key=os.getenv('ANTHROPIC_API_KEY'),
     )
     response = client.messages.create(
-        max_tokens = 100,
-        messages = [{
+        max_tokens=100,
+        messages=[{
             "role": "user",
-            "content": "What is the capital of France?"
+            "content": question
         }],
-        model = "claude-haiku-4-5"
+        model="claude-haiku-4-5"
     )
-    print(response)  # Print the response from the model
+    process_response(response)
+
+def process_response(response: Message):
+    print(f"Output From model: {response.content[-1].text}")
+    print(f"Input Tokens: {response.usage.input_tokens}")
+    print(f"Output Tokens: {response.usage.output_tokens}")
+
+
+def interact_with_model():
+    ask_claude("What is captial of France?")
+
+
+def get_weather(city: str) -> dict:
+    fake_weather_db = {
+        'hyderabad': {
+            'temp': '25',
+            'forecast': 'Flash flood risk',
+            'other_details': 'mostly cloudy'
+
+        },
+        'bangalore': {
+            'temp': '33',
+            'forecast': 'sunny',
+            'other_details': 'clear sky'
+        }
+    }
+    return fake_weather_db.get(city)
+
+def ask_claude_to_do_something():
+    load_dotenv()
+    client = Anthropic(
+        api_key=os.getenv('ANTHROPIC_API_KEY'),
+    )
+    tools_definition = [{
+        "name": "get_weather",
+        "description": "Retrieves the current temperature, weather forecast, and climate details for a specified city.",
+        "input_schema": {
+            "type": "object",
+            "properties": {
+            "city": {
+                "type": "string",
+                "description": "The name of the city to look up the weather for, e.g., 'hyderabad' or 'bangalore'."
+            }
+            },
+            "required": ["city"]
+        }
+    }]
+    messages = [
+        {
+            "role": "user", 
+            "content": "Do i need to carry umbrella for work in hyderabad?"}
+    ]
+    response = client.messages.create(
+        max_tokens=100,
+        messages=messages,
+        model="claude-haiku-4-5",
+        tools=tools_definition
+    )
+    #process_response(response)
+    print(response)
--- agenticai/openai_n_claude_api/main.py
+++ agenticai/openai_n_claude_api/main.py
@@ -4,17 +4,24 @@ import claude
 import gpt
 
 
-# def print_api_keys():
-#     load_dotenv()  # Load environment variables from .env file
-#     print(os.getenv("ANTHROPIC_API_KEY"))
-#     print(os.getenv("OPENAI_API_KEY"))
+def print_api_keys():
+    load_dotenv()
+    print(os.getenv('ANTHROPIC_API_KEY'))
+    print(os.getenv('OPENAI_API_KEY'))
 
 
-def interact_with_claude_model():
-    pass  # Placeholder for the function to interact with Claude model
 
+    
 
 if __name__ == "__main__":
     #print_api_keys()
-    claude.interact_with_model()
+    #claude.interact_with_model()
     #gpt.interact_with_model()
+
+    # claude.ask_claude("My name is Bharath")
+    # claude.ask_claude("What is my name?")
+    # gpt.ask_gpt("My name is Bharath")
+    # gpt.ask_gpt("What is my name?")
+
+    #claude.ask_claude_to_do_something()
+    gpt.ask_gpt_to_do_something()
